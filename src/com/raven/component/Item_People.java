b29023070ae679dafa1847d3e33406b5ea0d9623
Amaan Jambura <amaanjambura786@gmail.com>
2023-03-07 14:25:08 +0530

    Initial Commit and started designing of main chat screen

---
+++ src/com/raven/component/Item_People.java
@@ -0,0 +1,43 @@
+package com.raven.component;
+
+public class Item_People extends javax.swing.JPanel {
+
+    public Item_People(String name) {
+        initComponents();
+        lb.setText(name);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lb = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(229, 229, 229));
+
+        lb.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
+        lb.setText("Name");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lb, javax.swing.GroupLayout.DEFAULT_SIZE, 38, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lb;
+    // End of variables declaration//GEN-END:variables
+}
